74434b71de770bd761c7d8da1123936561222ad0
michal <michalchilczuk@gmail.com>
2014-01-02 22:46:56 +0100

    nowy wyjatek

---
+++ src/lift/server/exception/ServerSleepsExeption.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package lift.server.exception;
+
+/**
+ * @author Michal
+ * 
+ * Wyjatek oznaczajacy ze serwer nie pracuje.
+ *
+ */
+public class ServerSleepsExeption extends Exception
+{
+
+}
