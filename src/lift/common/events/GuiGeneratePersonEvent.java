08d372441c9a6c7b5e3929379af202e6611059e0
tmrozek <tmrozek1@gmail.com>
2014-01-04 20:15:16 +0100

    Update GuiGeneratePersonEvent.java
    Dodalem parametry ktore beda potrzebne w tym evencie

--- src/lift/common/events/GuiGeneratePersonEvent.java
+++ src/lift/common/events/GuiGeneratePersonEvent.java
@@ -1,6 +1,25 @@
 package lift.common.events;
 
-public class GuiGeneratePersonEvent extends LiftEvent
-{
+
+public class GuiGeneratePersonEvent extends LiftEvent{
+	
+	private final int homeFloor;
+	private final int destinationFloor;
+	
+	public GuiGeneratePersonEvent(final int homefloor, final int destinationfloor)
+	{
+		homeFloor = homefloor;
+		destinationFloor =destinationfloor;
+	}
+
+	public int getHomeFloor() {
+		return homeFloor;
+	}
+
+	public int getDestinationFloor() {
+		return destinationFloor;
+	}
+	
+	
 
 }
