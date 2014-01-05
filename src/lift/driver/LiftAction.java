bf11a49824e647c25c965ad4eceeee2f4c016b7e
Krzysiek <Krzysiek@192.168.2.2>
2014-01-05 14:16:33 +0100

    dodane komentarze do javadoc dla liftDriver

--- src/lift/driver/LiftAction.java
+++ src/lift/driver/LiftAction.java
@@ -3,7 +3,7 @@ package lift.driver;
 import lift.common.events.LiftEvent;
 
 /** An interface which has abstract method to execute proper 
- * methods after receiving particular event */
+ * action after receiving particular event */
 
 public interface LiftAction {
 	public abstract void execute(LiftEvent e);
