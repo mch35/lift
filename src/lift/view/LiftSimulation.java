7eb9a5b9e3fe593c365bd4b7820b6d4c04b9507e
wkaczma1 <kaczmar.w@gmail.com>
2014-01-06 01:01:52 +0100

    .

--- src/lift/view/LiftSimulation.java
+++ src/lift/view/LiftSimulation.java
@@ -76,7 +76,7 @@ public class LiftSimulation extends JFrame implements Runnable
    public LiftSimulation(final int iloscPieter, final Server server) throws ConnectionExitsException
    {  
 
-      building = new Building(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, Color.BLACK);
+      building = new Building(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, Color.BLACK, iloscPieter);
       shaft = new ElevatorShaft(CANVAS_WIDTH-IMAGE_WIDTH, 0, IMAGE_WIDTH, CANVAS_HEIGHT, Color.YELLOW);
       box = new ElevatorBox(CANVAS_WIDTH-IMAGE_WIDTH, CANVAS_HEIGHT-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_HEIGHT, Color.DARK_GRAY);
       
