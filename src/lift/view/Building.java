f07a4560f53f47893fb9567c98ad774661c926e6
wkaczma1 <kaczmar.w@gmail.com>
2014-01-06 01:00:53 +0100

    rysowanie tylu pieter ile jest w konstruktorze

--- src/lift/view/Building.java
+++ src/lift/view/Building.java
@@ -5,34 +5,36 @@ import java.awt.Graphics;
 
 
 public class Building {
-   // Variables (package access)
-   int x, y, width, height; // rectangle (for illustration)
-   Color color = Color.BLACK; // color of the object
-   
-   int floorNumber = 4;
+
+   int x, y, width, height; 
+   Color color;
+   int floorNumber;
  
    /** Constructor to setup the GUI */
-   public Building(int x, int y, int width, int height, Color color) {
+   public Building(int x, int y, int width, int height, Color color, int floorNumber)
+   {
       this.x = x;
       this.y = y;
       this.width = width;
       this.height = height;
       this.color = color;
+      this.floorNumber = floorNumber;
    }
  
    /** Paint itself (given the Graphics context) */
-   public void paint(Graphics g) {
+   public void paint(Graphics g)
+   {
       g.setColor(color);
       g.drawRect(0, 0, width, height);
       
       //createFloors
       int y_position = 0;
       String string = "FLOOR ";
-		for(int i=0; i<=floorNumber; ++i)
-		{
-			g.drawRect(0, y_position, 900, 126);
-			g.drawString(string+(floorNumber-i), 10, y_position+12);
-			y_position += 126;
-		}
+      for(int i=0; i<=floorNumber; ++i)
+      {
+    	  g.drawRect(0, y_position, 900, 126);
+    	  g.drawString(string+(floorNumber-i), 10, y_position+12);
+    	  y_position += 126;
+      }
    }
 }
